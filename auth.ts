43d678575e959dbe7503e5350f744f5c14c201e2
Chris Bell <chrisbell2358@gmail.com>
2024-02-21 08:29:57 +0000

    auth routes

--- auth.ts
+++ auth.ts
@@ -12,15 +12,12 @@ export const config = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      /*const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         return false; 
       } else {
         return true;
-      }*/
-
-      if (isLoggedIn) return true;
-      return false; // Redirect unauthenticated users to login page
+      }
 
     },
     async jwt({ token, user }) {
